bc2e58efb172a5783899d7054c40734361dea112
Алексей Бобков <153274834+AlexeyBobkovDev@users.noreply.github.com>
2026-07-23 11:22:25 +0300

    feat(TM-36, system): fix lefthook (#13)

--- scripts/check-formatting.sh
+++ scripts/check-formatting.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 
+set -euo pipefail
+
 FILES=("$@")
 
-test -z "$(gofmt -l ${FILES})"
-test -z "$(goimports -l ${FILES})"
-test -z "$(gofumpt -l ${FILES})"
-test -z "$(gci diff ${FILES})"
-test -z "$(golangci-lint run)"
+
+test -z "$(gofmt -l ${FILES[@]})"
+test -z "$(goimports -l ${FILES[@]})"
+test -z "$(gofumpt -l ${FILES[@]})"
+test -z "$(gci diff ${FILES[@]})"
+
+for service in services/*; do
+	(
+		cd "$service"
+		for file in ${FILES[@]}; do
+			if [[ "$file" != "$service/"* ]]; then
+				continue
+			fi
+
+			file="${file#$service/}"
+			
+			test -z "$(golangci-lint run "$file")"
+		done
+	)
+done
+
