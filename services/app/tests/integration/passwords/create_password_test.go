f1de52280f7587ff4af002881218c8b7a4f61892
Алексей Бобков <153274834+AlexeyBobkovDev@users.noreply.github.com>
2026-07-27 19:55:08 +0300

    feat(TM-41, app): fix tests/integration/create_password_test.go (#23)
    Now it corresponds pool.NewPool func

--- services/app/tests/integration/passwords/create_password_test.go
+++ services/app/tests/integration/passwords/create_password_test.go
@@ -8,13 +8,14 @@ import (
 	"path/filepath"
 	"testing"
 
+	core_postgres_pool "github.com/AlexeyBobkovDev/tripmate/services/app/internal/core/repository/postgres/pool"
+	core_pgx_pool "github.com/AlexeyBobkovDev/tripmate/services/app/internal/core/repository/postgres/pool/pgx"
 	passwords_api "github.com/AlexeyBobkovDev/tripmate/services/app/internal/features/passwords/api"
 	passwords_postgres_repository "github.com/AlexeyBobkovDev/tripmate/services/app/internal/features/passwords/repository/postgres"
 	passwords_service "github.com/AlexeyBobkovDev/tripmate/services/app/internal/features/passwords/service"
 	"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
-	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/modules/postgres"
@@ -69,7 +70,7 @@ func TestCreatePassword(t *testing.T) {
 	require.NotEqual(t, passwordToHash, password.Hash, "passwordToHash must not be equal to hashed password")
 }
 
-func SetupTestDB(t *testing.T, ctx context.Context) (*pgxpool.Pool, string) {
+func SetupTestDB(t *testing.T, ctx context.Context) (core_postgres_pool.Pool, string) {
 	t.Helper()
 
 	containerDB, err := postgres.Run(
@@ -88,7 +89,7 @@ func SetupTestDB(t *testing.T, ctx context.Context) (*pgxpool.Pool, string) {
 	connString, err := containerDB.ConnectionString(ctx, "sslmode=disable")
 	require.NoError(t, err)
 
-	pool, err := pgxpool.New(ctx, connString)
+	pool, err := core_pgx_pool.NewPool(ctx, connString, nil)
 	require.NoError(t, err)
 
 	t.Cleanup(func() { pool.Close() })
@@ -96,7 +97,7 @@ func SetupTestDB(t *testing.T, ctx context.Context) (*pgxpool.Pool, string) {
 	return pool, connString
 }
 
-func RunMigrations(t *testing.T, db *pgxpool.Pool, connString string) {
+func RunMigrations(t *testing.T, db core_postgres_pool.Pool, connString string) {
 	t.Helper()
 
 	migrationsRoot := getMigrationsRoot()
