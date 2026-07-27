a8d113dc65ec8bf644f2a0507a2df71986be2c07
Alexey Bobkov <alexeybobkovdev@outlook.com>
2026-07-27 18:54:40 +0300

    feat(TM-35, app): setup response proccessing for http transport

---
+++ services/app/internal/core/transport/http/response/handler.go
@@ -0,0 +1,77 @@
+package core_http_response
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	core_errors "github.com/AlexeyBobkovDev/tripmate/services/app/internal/core/errors"
+	core_logger "github.com/AlexeyBobkovDev/tripmate/services/app/internal/core/logger"
+	"go.uber.org/zap"
+)
+
+type HTTPResponseHandler struct {
+	rw  http.ResponseWriter
+	log *core_logger.Logger
+}
+
+func NewHTTPResponseHandler(
+	rw http.ResponseWriter,
+	log *core_logger.Logger,
+) *HTTPResponseHandler {
+	return &HTTPResponseHandler{
+		rw:  rw,
+		log: log,
+	}
+}
+
+func (h *HTTPResponseHandler) JSONResponse(
+	statusCode int,
+	responseBody any,
+) {
+	h.rw.WriteHeader(statusCode)
+	if err := json.NewEncoder(h.rw).Encode(responseBody); err != nil {
+		h.log.Error("write HTTP response", zap.Error(err))
+	}
+}
+
+func (h *HTTPResponseHandler) ErrorResponse(err error, msg string) {
+	var (
+		statusCode int
+		logFunc    func(string, ...zap.Field)
+	)
+
+	switch {
+	case errors.Is(err, core_errors.ErrInvalidArgument):
+		statusCode = http.StatusBadRequest
+		logFunc = h.log.Warn
+	case errors.Is(err, core_errors.ErrNotFound):
+		statusCode = http.StatusNotFound
+		logFunc = h.log.Debug
+	case errors.Is(err, core_errors.ErrConflict):
+		statusCode = http.StatusConflict
+		logFunc = h.log.Warn
+	default:
+		statusCode = http.StatusInternalServerError
+		logFunc = h.log.Error
+	}
+
+	logFunc(msg, zap.Error(err))
+	h.errorResponse(statusCode, err, msg)
+}
+
+func (h *HTTPResponseHandler) errorResponse(
+	statusCode int,
+	err error,
+	msg string,
+) {
+	response := ErrorResponse{
+		Error:   err.Error(),
+		Message: msg,
+	}
+
+	h.JSONResponse(
+		statusCode,
+		response,
+	)
+}
