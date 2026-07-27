6075edc34469f86a3beaae3256505aa14b53cf35
Alexey Bobkov <alexeybobkovdev@outlook.com>
2026-07-28 01:36:04 +0300

    fix(TM-49, service): fix check-formating.sh

--- scripts/check-formatting.sh
+++ scripts/check-formatting.sh
@@ -4,24 +4,31 @@ set -euo pipefail
 
 FILES=("$@")
 
-
-test -z "$(gofmt -l ${FILES[@]})"
-test -z "$(goimports -l ${FILES[@]})"
-test -z "$(gofumpt -l ${FILES[@]})"
-test -z "$(gci diff ${FILES[@]})"
+test -z "$(gofmt -l "${FILES[@]}")" || echo "$(gofmt -l "${FILES[@]}")"
+test -z "$(goimports -l "${FILES[@]}")" || echo "$(goimports -l "${FILES[@]}")"
+test -z "$(gofumpt -l "${FILES[@]}")" || echo "$(gofumpt -l "${FILES[@]}")"
+test -z "$(
+	gci diff \
+		--custom-order \
+		-s standard \
+		-s default \
+		-s "prefix(github.com/AlexeyBobkovDev/tripmate)" \
+		"${FILES[@]}"
+)" || {
+	echo "$(gci diff \
+		--custom-order \
+		-s standard \
+		-s default \
+		-s "prefix(github.com/AlexeyBobkovDev/tripmate)" \
+		"${FILES[@]}")"
+	exit 1
+}
 
 for service in services/*; do
+	echo "$service"
 	(
 		cd "$service"
-		for file in ${FILES[@]}; do
-			if [[ "$file" != "$service/"* ]]; then
-				continue
-			fi
 
-			file="${file#$service/}"
-
-			test -z "$(golangci-lint run "$file")"
-		done
+		golangci-lint run ./...
 	)
 done
-
