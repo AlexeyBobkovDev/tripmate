a8c21546047b9a9bf73ca6e72595fc701f6d1de4
Alexey Bobkov <alexeybobkovdev@outlook.com>
2026-08-04 09:51:14 +0000

    fix(TM-54, service): fix files look up
    Now scripts are more wise in how they determine which files should be
    checked

--- scripts/check-scripts.sh
+++ scripts/check-scripts.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
-output="$(shellcheck -x scripts/*.sh)"
+SH_FILES=$(printf "%s\n" "$@" | grep -E '\.sh$' || true)
+
+if [[ "${#SH_FILES[@]}" != 0 ]]; then
+	output="$(shellcheck -x "${SH_FILES[@]}")"
+else
+	output="$(shellcheck -x scripts/*.sh)"
+fi
 
 test -z "$output" || echo "$output"
