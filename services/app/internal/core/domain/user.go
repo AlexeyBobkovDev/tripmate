21ae359cc92cc4b9180151f4a81df8aaff4bb751
Alexey Bobkov <alexeybobkovdev@outlook.com>
2026-09-04 21:54:41 +0000

    feat(TM-70, app): implement Nullable type and UserPatch struct

--- services/app/internal/core/domain/user.go
+++ services/app/internal/core/domain/user.go
@@ -1,6 +1,9 @@
 package domain
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type User struct {
 	ID          int
@@ -15,6 +18,49 @@ type User struct {
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 	DeletedAt   *time.Time
+}
+
+func (u *User) Validate() error {
+	return nil
+}
+
+func (u *User) ApplyPatch(patch *UserPatch) error {
+	if err := patch.Validate(); err != nil {
+		return fmt.Errorf("validate user patch: %w", err)
+	}
+
+	tmp := *u
+
+	tmp.Version = patch.Version
+	if patch.Name.Set {
+		tmp.Name = *patch.Name.Value
+	}
+	if patch.Surname.Set {
+		tmp.Surname = *patch.Surname.Value
+	}
+	if patch.Username.Set {
+		tmp.Username = *patch.Username.Value
+	}
+	if patch.BirthDate.Set {
+		tmp.BirthDate = *patch.BirthDate.Value
+	}
+	if patch.Description.Set {
+		tmp.Description = *patch.Description.Value
+	}
+	if patch.Email.Set {
+		tmp.Email = *patch.Email.Value
+	}
+	if patch.PhoneNumber.Set {
+		tmp.PhoneNumber = *patch.PhoneNumber.Value
+	}
+
+	if err := tmp.Validate(); err != nil {
+		fmt.Errorf("validate patched user: %w", err)
+	}
+
+	*u = tmp
+
+	return nil
 }
 
 func NewUser(
@@ -67,5 +113,42 @@ func NewUserUninitialized(
 		Email:       email,
 		PhoneNumber: phoneNumber,
 		DeletedAt:   nil,
+	}
+}
+
+type UserPatch struct {
+	Version     int
+	Name        Nullable[string]
+	Surname     Nullable[string]
+	Username    Nullable[string]
+	BirthDate   Nullable[time.Time]
+	Description Nullable[string]
+	Email       Nullable[string]
+	PhoneNumber Nullable[string]
+}
+
+func (u *UserPatch) Validate() error {
+	return nil
+}
+
+func NewUserPatch(
+	version int,
+	name Nullable[string],
+	surname Nullable[string],
+	username Nullable[string],
+	birthDate Nullable[time.Time],
+	description Nullable[string],
+	email Nullable[string],
+	phoneNumber Nullable[string],
+) *UserPatch {
+	return &UserPatch{
+		Version:     version,
+		Name:        name,
+		Surname:     surname,
+		Username:    username,
+		BirthDate:   birthDate,
+		Description: description,
+		Email:       email,
+		PhoneNumber: phoneNumber,
 	}
 }
