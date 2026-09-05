c828a0305c67338361ef3f7eeb56e85be5becb24
Alexey Bobkov <alexeybobkovdev@outlook.com>
2026-09-05 15:47:33 +0000

    feat(TM-70, app): delete app.passwords table
    NOW PASSWORDS ARE STORED ONLY AS HASHES IN app.users TABLE

--- services/app/migrations/000001_init.up.sql
+++ services/app/migrations/000001_init.up.sql
@@ -5,11 +5,12 @@ CREATE TABLE IF NOT EXISTS app.users (
     version BIGINT NOT NULL DEFAULT 1,
     name VARCHAR(50) NOT NULL,
     surname VARCHAR(50) NOT NULL,
-    username VARCHAR(100) NOT NULL UNIQUE,
+    username VARCHAR(100) NOT NULL,
     birth_date DATE NOT NULL,
     description VARCHAR(1000) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    phone_number VARCHAR(16) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL,
+    phone_number VARCHAR(16) NOT NULL,
+    password_hash TEXT NOT NULL UNIQUE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     deleted_at TIMESTAMPTZ,
@@ -18,14 +19,19 @@ CREATE TABLE IF NOT EXISTS app.users (
     CONSTRAINT users_surname_check CHECK(CHAR_LENGTH(TRIM(surname)) > 0),
     CONSTRAINT users_username_check CHECK(CHAR_LENGTH(TRIM(username)) > 0),
     CONSTRAINT users_birth_date_check CHECK (
-        EXTRACT(YEAR FROM age(NOW(), birth_date)) BETWEEN 14 AND 150
+        EXTRACT(
+            YEAR
+            FROM
+                age(NOW(), birth_date)
+        ) BETWEEN 14
+        AND 150
     ),
     CONSTRAINT users_description_check CHECK (CHAR_LENGTH(TRIM(description)) > 0),
     CONSTRAINT users_email_check CHECK (email ~ '^[^@\s]+@[^@\s]+\.[^@\s]+$'),
     CONSTRAINT users_phone_number_check CHECK (
-        CHAR_LENGTH(phone_number) BETWEEN 10 AND 16
-        AND
-        phone_number ~ '^\+[0-9]{10,15}$'
+        CHAR_LENGTH(phone_number) BETWEEN 10
+        AND 16
+        AND phone_number ~ '^\+[0-9]{10,15}$'
     ),
     CONSTRAINT users_updated_at_check CHECK (updated_at >= created_at),
     CONSTRAINT users_deleted_at_check CHECK (
@@ -34,13 +40,14 @@ CREATE TABLE IF NOT EXISTS app.users (
     )
 );
 
-CREATE TABLE IF NOT EXISTS app.passwords (
-    user_id BIGSERIAL PRIMARY KEY REFERENCES app.users(id),
-    version BIGINT NOT NULL DEFAULT 1,
-    password_hash BYTEA NOT NULL,
-    salt BYTEA NOT NULL,
-    times INT NOT NULL CHECK(times >= 1),
-    memory INT NOT NULL CHECK(memory >= 1024),
-    threads INT NOT NULL CHECK(threads >= 1),
-    key_len INT NOT NULL CHECK(key_len > 1)
-)
+CREATE UNIQUE INDEX idx_unique_user_username ON app.users(username)
+WHERE
+    deleted_at IS NULL;
+
+CREATE UNIQUE INDEX idx_unique_user_email ON app.users(email)
+WHERE
+    deleted_at IS NULL;
+
+CREATE UNIQUE INDEX idx_unique_user_phone_number ON app.users(phone_number)
+WHERE
+    deleted_at IS NULL;
